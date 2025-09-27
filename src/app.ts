486b58ab93edeb7d45f0c56c40506ccaa96febd0
fredson <fredsouza.dev@gmail.com>
2025-09-27 11:16:42 -0400

    implement refresh token

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,28 @@
 import fastify from 'fastify'
+import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
 import { haircutsRoutes } from './http/controllers/haircuts/routes'
 import z, { ZodError } from 'zod'
 import { env } from './env'
 import { barberShopsRoutes } from './http/controllers/barber-shops/routes'
 import { barberCustomersRoutes } from './http/controllers/baber-customers/routes'
 import { checkInsRoutes } from './http/controllers/check-ins/routes'
-import fastifyJwt from '@fastify/jwt'
 import { userRoutes } from './http/controllers/users/routes'
 
 export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
 })
+
+app.register(fastifyCookie)
 
 app.register(haircutsRoutes)
 app.register(barberShopsRoutes)
